8d943e5c47e1b7d125852ad720b2dc8a582cfbd9
Alejandro Brozzo <alejandro.brozzo@wunderdog.fi>
2026-03-22 21:46:04 +0100

    lint and format

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+// @ts-check
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    ignores: ['dist/', 'node_modules/'],
+  },
+);
